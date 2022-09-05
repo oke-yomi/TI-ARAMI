6b9a682ad36c44b74cb5cfafc9fcb0c00b7b9bda
oke-yomi <okeoluwayomi9@gmail.com>
2022-09-05 17:11:26 +0100

    tailwin re setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
 import store from "@/store";
-import "@/assets/tailwind.css";
 import router from "@/router";
 
 createApp(App).use(router).use(store).mount("#app");
